0581d74ee26b31ee55c02e2948a18933a24977df
Alice-anjali <alice.anjali1995@gmail.com>
2017-07-16 14:35:28 +0530

    add probot

--- server.js
+++ server.js
@@ -1,57 +1,74 @@
-var dotenv = require('dotenv');
+// var dotenv = require('dotenv');
+//
+// dotenv.load();
+// var GitHub = require('github-api');
+// var get_token = require('./TOKEN');
+// var request = require('request');
+// var validateMessage = require('validate-commit-msg');
+//
+// var gh = new GitHub({
+//       token : get_token
+// });
+//
+// const secret = process.env.WEBHOOK_SECRET;
+//
+// const http = require('http')
+// const webHookHandler = require('github-webhook-handler')({
+//   path: '/',
+//   secret: secret
+// })
+// http.createServer(handleRequest).listen(8080)
+// console.log('server started')
+//
+// webHookHandler.on('pull_request', (event) => {
+//   console.log(`Received PR "${event.payload.pull_request.number}"`);
+//   console.log(`Commits URL "${event.payload.pull_request.commits_url}"`);
+//
+//   request({
+//     url : event.payload.pull_request.commits_url,
+//     headers : {
+//       'User-Agent': 'Hoodie Bot'
+//     }
+//   }, (error, response,body) => {
+//     var commits = JSON.parse(body);
+//     var commitMessages = [];
+//     for(var i = 0; i < commits.length; i++) {
+//       commitMessages.push(commits[i].commit.message);
+//     }
+//     // console.log(commitMessages);
+//     // var valid = validateMessage('chore(index): an example commit message');
+//     for(var i = 0; i < commitMessages.length; i++) {
+//       console.log(commitMessages[i]);
+//       var valid = validateMessage(commitMessages[i]);
+//       console.log(valid);
+//     }
+//   });
+// })
+//
+// function handleRequest (request, response) {
+//   // ignore all requests that aren’t POST requests
+//   if (request.method !== 'POST') {
+//     console.log("Get Request Received");
+//     return response.end('ok');
+//   }
+//   // here we pass the current request & response to the webHookHandler we created
+//   // on top. If the request is valid, then the "issue" above handler is called
+//   webHookHandler(request, response, () => response.end('ok'))
+// }
 
-dotenv.load();
-var GitHub = require('github-api');
-var get_token = require('./TOKEN');
-var request = require('request');
-var validateMessage = require('validate-commit-msg');
 
-var gh = new GitHub({
-      token : get_token
-});
+module.exports = function (robot) {
+  robot.on('issues', handleIssue.bind(null, robot))
+}
 
-const secret = process.env.WEBHOOK_SECRET;
+function handleIssue (robot, context) {
+  const api = context.github
+  const {installation, repository, issue} = context.payload
 
-const http = require('http')
-const webHookHandler = require('github-webhook-handler')({
-  path: '/',
-  secret: secret
-})
-http.createServer(handleRequest).listen(8080)
-console.log('server started')
-
-webHookHandler.on('pull_request', (event) => {
-  console.log(`Received PR "${event.payload.pull_request.number}"`);
-  console.log(`Commits URL "${event.payload.pull_request.commits_url}"`);
-
-  request({
-    url : event.payload.pull_request.commits_url,
-    headers : {
-      'User-Agent': 'Hoodie Bot'
-    }
-  }, (error, response,body) => {
-    var commits = JSON.parse(body);
-    var commitMessages = [];
-    for(var i = 0; i < commits.length; i++) {
-      commitMessages.push(commits[i].commit.message);
-    }
-    // console.log(commitMessages);
-    // var valid = validateMessage('chore(index): an example commit message');
-    for(var i = 0; i < commitMessages.length; i++) {
-      console.log(commitMessages[i]);
-      var valid = validateMessage(commitMessages[i]);
-      console.log(valid);
-    }
-  });
-})
-
-function handleRequest (request, response) {
-  // ignore all requests that aren’t POST requests
-  if (request.method !== 'POST') {
-    console.log("Get Request Received");
-    return response.end('ok');
-  }
-  // here we pass the current request & response to the webHookHandler we created
-  // on top. If the request is valid, then the "issue" above handler is called
-  webHookHandler(request, response, () => response.end('ok'))
+  api.issues.createComment({
+    owner: repository.owner.login,
+    repo: repository.name,
+    number: issue.number,
+    body: 'Welcome to the robot uprising.'
+  })
 }
