02fabe3ca6acef373d5349094916aa9e18d832b6
Alice-anjali <alice.anjali1995@gmail.com>
2017-07-17 12:39:32 +0530

    fix: probot bug

--- server.js
+++ server.js
@@ -57,18 +57,28 @@
 // }
 
 
-module.exports = function (robot) {
-  robot.on('issues', handleIssue.bind(null, robot))
-}
+// module.exports = function (robot) {
+//   robot.on('issues', handleIssue.bind(null, robot))
+// }
+//
+// function handleIssue (robot, context) {
+//   const api = context.github
+//   const {installation, repository, issue} = context.payload
+//
+//   api.issues.createComment({
+//     owner: repository.owner.login,
+//     repo: repository.name,
+//     number: issue.number,
+//     body: 'Welcome to the robot uprising.'
+//   })
+// }
 
-function handleIssue (robot, context) {
-  const api = context.github
-  const {installation, repository, issue} = context.payload
 
-  api.issues.createComment({
-    owner: repository.owner.login,
-    repo: repository.name,
-    number: issue.number,
-    body: 'Welcome to the robot uprising.'
-  })
-}
+module.exports = robot => {
+  robot.on('issues.opened', context => {
+    const params = context.issue({
+      body: 'Hello World!'
+    });
+    context.github.issues.createComment(params);
+  });
+};
